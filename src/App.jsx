6e971f696ac3f802627ca317e219ff5e111bf3b4
Muneerat <oladipomunirat@gmail.com>
2024-03-25 16:40:49 +0100

    add product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,145 +1,202 @@
 //import { useState, useContext } from 'react';
-import { BrowserRouter as Router, Link, Routes, Route} from 'react-router-dom';
-import { AppContext } from './Contexts/AppContent';
-import NavBar from './Components/NavBar';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import { AppContext } from "./Contexts/AppContent";
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 // import SignUp from './Pages/SignUp';
-import axios from 'axios';
-import SingleProduct from './Pages/SingleProduct';
-import { useEffect, useState } from 'react';
-import Footer from './Components/Footer';
-import SignUp from './Pages/Auth/SignUp';
-import SignIn from './Pages/Auth/SignIn';
+import axios from "axios";
+import SingleProduct from "./Pages/SingleProduct";
+import { useEffect, useState } from "react";
+import Footer from "./Components/Footer";
+import SignUp from "./Pages/Auth/SignUp";
+import SignIn from "./Pages/Auth/SignIn";
 // import { ToastContainer, toast } from 'react-toastify';
 //   import 'react-toastify/dist/ReactToastify.css';
- import toast, {Toaster} from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
+import AllProduct from "./Components/AllProduct";
 //import toast,{ ToastContainer } from 'react-toastify';
 // import {Toaster} from 'react-hot-toast';
 
 function App() {
   // const [loading, setLoading] = useState(false);
- const [cart, setCart] = useState([]);
- const [notice, setNotice] = useState({});
+  const [cart, setCart] = useState([]);
+  const [notice, setNotice] = useState({});
+  const [errors, setErrors] = useState({});
+  const [darkMode, setDarkMode] = useState(false);
+  const [ourProducts, setOurProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
 
-//  console.log(cart);
-//Get the items from the cart
- const getCart = async () => {
-  try {
-    const response = await axios.get(`https://fakestoreapi.com/carts/5`); 
-    console.log(response.data);
-    setCart(response.data.products); 
-    // console.log(response.data.product);
-  } catch (error) {
-    console.error('Error fetching cart:', error);
-  }
- }
+  const validate = () => {
+    const errors = {};
+    if (!name) {
+      errors.name = "Name is required";
+    }
+    if (!email) {
+      errors.email = "Email is required";
+    }
+    if (!password) {
+      errors.password = "Password is required";
+    }
+    if (!username) {
+      errors.username = "Username is required";
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+  //Get all products
+  const getOurProducts = () => {
+    setLoading(true);
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setOurProducts(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  useEffect(() => {
+    getOurProducts();
+  }, []);
+  //  console.log(cart);
+  //Get the items from the cart
+  const getCart = async () => {
+    try {
+      const response = await axios.get(`https://fakestoreapi.com/carts/3`);
+      console.log(response.data);
+      setCart(response.data.products);
+      // console.log(response.data.product);
+    } catch (error) {
+      console.error("Error fetching cart:", error);
+    }
+  };
 
-// get existing cart
-// check if product exists in cart and increase the qty
-// if it doesn\t, add product
-// submit new to api
-// if it doesn't exist add product to cart and if it exist increment qty to current quantity number 
- const addToCart = async (product) => {
-  let existingCart = cart ?? []
-  let productExist = existingCart.find((prod) => prod.id = product.id)
-  // console.log(productExist)
+  // get existing cart
+  // check if product exists in cart and increase the qty
+  // if it doesn\t, add product
+  // submit new to api
+  // if it doesn't exist add product to cart and if it exist increment qty to current quantity number
+  const addToCart = async (product) => {
+    let existingCart = cart ?? [];
+    let productExist = existingCart.find((prod) => (prod.id = product.id));
+    // console.log(productExist)
 
-  if (productExist) {
-    productExist.quantity += 1
-    let index = existingCart.findIndex(prod => prod.id === product.id)
-    existingCart[index] = productExist
-  }
-  else {
-    existingCart.push({
-      productid: product.id,
-      quantity: 1
-  })
-  }
-  try {
-    // let response = await axios.post('https://fakestoreapi.com/carts', JSON.stringify(payload));
-    let response = await axios.post('https://fakestoreapi.com/carts', {
-      userid: '5',
-      date: (new Date).getDate(),
-      products: existingCart
-    });
-    // console.log(response.data);
-    setCart(response.data);
-    getCart()
-  } catch (error) {
-    console.error('Error adding to cart:', error);
-  }
-  
-  //  let existingProduct = cart.find((productID)  => productID.id === productID.id);
-  //  console.log("GOT HERE")
-  //  if(existingProduct) {
-  //   let index = cart.findIndex((productID) => productID.id === productID.id);
-  //   let newCart = [...cart];
+    if (productExist) {
+      productExist.quantity += 1;
+      let index = existingCart.findIndex((prod) => prod.id === product.id);
+      existingCart[index] = productExist;
+    } else {
+      existingCart.push({
+        productid: product.id,
+        quantity: 1,
+      });
+    }
+    try {
+      // let response = await axios.post('https://fakestoreapi.com/carts', JSON.stringify(payload));
+      let response = await axios.post("https://fakestoreapi.com/carts", {
+        userid: "5",
+        date: new Date().getDate(),
+        products: existingCart,
+        // title: title,
+        // price: price,
+        // description: description,
+        // image: image,
+        // category: category,
 
-  //   newCart[index].qty = existingProduct.qty + 1;
-  //   setCart(newCart);
-  //  } else {
-  //    product.qty = 1;
-  //    setCart((prev) => [...prev, product])
-  //  }
- }
- useEffect(() => {
-   getCart();
-   if(notice.message){
-        if(notice.type == 'success'){
-          toast.success(notice.message, {
-            position: 'top-right'
-          });
-        }
-        else{
-          toast.error(notice.message, {
-            position: 'top-right'
-          });
-        }
+      });
+      console.log(response.data);
+      setCart(response.data);
+      getCart();
+    } catch (error) {
+      console.error("Error adding to cart:", error);
+    }
+
+    //  let existingProduct = cart.find((productID)  => productID.id === productID.id);
+    //  console.log("GOT HERE")
+    //  if(existingProduct) {
+    //   let index = cart.findIndex((productID) => productID.id === productID.id);
+    //   let newCart = [...cart];
+
+    //   newCart[index].qty = existingProduct.qty + 1;
+    //   setCart(newCart);
+    //  } else {
+    //    product.qty = 1;
+    //    setCart((prev) => [...prev, product])
+    //  }
+  };
+  useEffect(() => {
+    getCart();
+    if (notice.message) {
+      if (notice.type == "success") {
+        toast.success(notice.message, {
+          position: "top-right",
+        });
+      } else {
+        toast.error(notice.message, {
+          position: "top-right",
+        });
       }
- },[notice])
-//  useEffect(() => {
-//   if(notice.message){
-//     if(notice.type === 'success'){
-//       toast.success(notice.message, {
-//         position: 'top-right'
-//       });
-//     }
-//     else{
-//       toast.error(notice.message, {
-//         position: 'top-right'
-//       });
-//     }
-//   }
-// },[notice])
- 
+    }
+  }, [notice]);
+  //  useEffect(() => {
+  //   if(notice.message){
+  //     if(notice.type === 'success'){
+  //       toast.success(notice.message, {
+  //         position: 'top-right'
+  //       });
+  //     }
+  //     else{
+  //       toast.error(notice.message, {
+  //         position: 'top-right'
+  //       });
+  //     }
+  //   }
+  // },[notice])
+
   return (
-    <AppContext.Provider
-     value={{ 
-       cart,
-       setCart,
-       addToCart: addToCart,
-       setNotice
-      }}>
-      {/* <Toaster/> */}
-      {/* <ToastContainer />  */}
-       <Toaster />  
-      <Router>
-      <NavBar />
-       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path=':id' element={<SingleProduct/>} />
-       </Routes>
-       <Footer />
-      </Router>
-    
-    </AppContext.Provider>
-  )
+    <div className={darkMode ? "app dark" : "dark"}>
+      <AppContext.Provider
+        value={{
+          cart,
+          setCart,
+          addToCart: addToCart,
+          setNotice,
+          errors,
+          validate,
+          darkMode,
+          toggleTheme,
+          ourProducts,
+          setOurProducts,
+          getOurProducts,
+        }}
+      >
+        {/* <Toaster/> */}
+        {/* <ToastContainer />  */}
+        <Toaster />
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path=":id" element={<SingleProduct />} />
+            <Route path="/allProducts" element={<AllProduct />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </AppContext.Provider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Pages/Auth/SignIn.jsx
+++ src/Pages/Auth/SignIn.jsx
@@ -6,6 +6,8 @@ import { Link, useNavigate } from "react-router-dom";
 import FormLabel from "../../Components/Form/FormLabel";
 import Layouts from "../../Components/Layouts";
 import axios from "axios";
+import FormError from "../../Components/Form/FormError";
+import { AppContext } from "../../Contexts/AppContent";
 //import { AppContext } from "../../Contexts/AppContent";
 
 
@@ -14,10 +16,29 @@ export default function SignIn() {
   const navigate = useNavigate();
   const [password, SetPassword] = useState("");
   const [username, SetUsername] = useState("");
-  //const {setNotice} = useContext(AppContext);
+  const [errors, setErrors] = useState({})
+  //  const errors = useContext(AppContext);
+   //const {validate} = useContext(AppContext);
 
+  //const {setNotice} = useContext(AppContext);
+  const validate = () => {
+    const errors = {};
+    if (!password && password.trim()) {
+      errors.password = "Password is required";
+    }
+    if (!username) {
+      errors.username = "Username is required";
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  }
   const submit = (e) => {
     e.preventDefault();
+    const isValid = validate();
+    if(!isValid){
+      return
+    }
+    //setErrors({});
     setSubmitting(true);
     
     axios
@@ -65,7 +86,7 @@ export default function SignIn() {
         </div>
         <div className="flex flex-col justify-center  align-middle mx-auto">
           <form onSubmit={submit}>
-            <h2 className="py-3 text-4xl">LogIn</h2>
+            <h2 className="py-3 text-4xl">Login</h2>
             <p className="py-3 px-1">Enter your details below</p>
             <div className="max-w-xs mx-auto my-3">
               <FormLabel>Enter your Username</FormLabel>
@@ -74,8 +95,10 @@ export default function SignIn() {
                 name="username"
                 id="username"
                 value={username}
+                
                 onChange={(e) => SetUsername(e.target.value)}
               />
+               <FormError error={errors?.username} />
             </div>
             <div className="max-w-xs mx-auto my-3">
               <FormLabel>Enter your Password</FormLabel>
@@ -83,10 +106,12 @@ export default function SignIn() {
                 type="password"
                 placeholder="********"
                 name="password"
+                autoComplete
                 id="password"
                 value={password}
                 onChange={(e) => SetPassword(e.target.value)}
               />
+               <FormError error={errors?.password} />
             </div>
             <Button
               className="bg-primary w-full text-white my-4 hover:text-black hover:bg-primary-light disabled:opacity-50 disabled:cursor-not-allowed"
@@ -95,8 +120,11 @@ export default function SignIn() {
               {submitting && (
                 <div className="w-5 h-5 rounded-full animate-spin border-r border-t border-slate-800"></div>
               )}
-              {!submitting && <span>Create Account</span>}
+              {!submitting && <span>Login</span>}
             </Button>
+            <p>
+              New here? <Link to="/signup" className=" text-primary"> Sign up</Link>
+            </p>
           </form>
         
         </div>
